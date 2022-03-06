07f80082e17b9872c6fb0d458c998f98ea4bdbcd
josswe26 <josec.sanchez@icloud.com>
2022-03-06 23:12:12 +0000

    Create database models

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+SCORE = ((1, 'Upvote'), (-1, 'Downvote'))
+
+
+class Question(models.Model):
+    """ Question model """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_questions')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    votes_score = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+        def __str__(self):
+            return self.title
+
+
+class Reply(models.Model):
+    """ Reply model """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='replies')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_replies')
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    votes_count = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+        def __str__(self):
+            return self.title
+
+
+class QuestionVote(models.Model):
+    """ Question votes model """
+    voter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_question_votes')
+    score = models.IntegerField(choices=SCORE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question_votes')
+
+
+class ReplyVote(models.Model):
+    """ Reply votes model """
+    voter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reply_votes')
+    score = models.IntegerField(choices=SCORE)
+    reply = models.ForeignKey(Reply, on_delete=models.CASCADE, related_name='reply_votes')
+    
