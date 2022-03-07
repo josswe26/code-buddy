e3804708ff2bdd8b9341e0bc3e8d00e48c33a030
josswe26 <josec.sanchez@icloud.com>
2022-03-07 01:23:12 +0000

    Create admin site

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Question, Reply
 
-# Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'author', 'created_on', 'last_updated')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created_on', 'last_updated')
+    search_fields = ('title', 'content', 'author__username')
+
+
+@admin.register(Reply)
+class ReplyAdmin(admin.ModelAdmin):
+
+    list_display = ('body', 'author', 'created_on', 'last_updated')
+    list_filter = ('created_on', 'last_updated')
+    search_fields = ('body', 'author__username')
