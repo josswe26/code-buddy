8be3cef4910016dbb21cac112d97a7e51e3b4ae5
josswe26 <josec.sanchez@icloud.com>
2022-03-07 22:25:20 +0000

    Create QuestionListView code

--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import list
+from .models import Question
 
-# Create your views here.
+
+class QuestionListView(ListView):
+
+    model = Question
+    paginate_by = 10
+
+    queryset = Question.objects.all().order_by('-created.on')
+    template_name = 'index.html'
