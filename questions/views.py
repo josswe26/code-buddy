ffb8cb25a2238bab0927e9778fef5850cc84eda6
josswe26 <josec.sanchez@icloud.com>
2022-03-10 23:40:41 +0000

    Fix ListView import and QuestionListView code in views.py

--- questions/views.py
+++ questions/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import list
+from django.views import generic
 from .models import Question
 
 
-class QuestionListView(ListView):
+class QuestionListView(generic.ListView):
 
     model = Question
     paginate_by = 10
 
-    queryset = Question.objects.all().order_by('-created.on')
+    queryset = Question.objects.all().order_by('-created_on')
     template_name = 'index.html'
