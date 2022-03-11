b614253b89fb42b283deaa38f6a953183a741eb0
josswe26 <josec.sanchez@icloud.com>
2022-03-11 23:12:11 +0000

    Create QuestionDetailView code

--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Question
 
 
@@ -10,3 +10,21 @@ class QuestionListView(generic.ListView):
 
     queryset = Question.objects.all().order_by('-created_on')
     template_name = 'index.html'
+
+
+class QuestionDetailView(View):
+
+    def get(self, request, slug, *args, **kwargs):
+
+        queryset = Question.objects.all()
+        question = get_object_or_404(queryset, slug=slug)
+        replies = question.replies.all().order_by('created_on')
+        
+        return render(
+            request,
+            'question_detail.html',
+            {
+                'question': question,
+                "replies": replies,
+            }
+        )
