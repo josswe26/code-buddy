1610a890550853fc82e4b619e8e5f2c2f3bceae9
josswe26 <josec.sanchez@icloud.com>
2022-03-11 23:15:31 +0000

    Connect URL for QuestionDetailView

--- questions/urls.py
+++ questions/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.QuestionListView.as_view(), name='home'),
+    path('<slug:slug>/', views.QuestionDetailView.as_view(), name='question_detail')
 ]
