9a70fdc712043482f68a55e8a164cf94a6ee7a36
josswe26 <josec.sanchez@icloud.com>
2022-03-12 00:03:33 +0000

    Install and configure django-allauth

--- code_buddy/urls.py
+++ code_buddy/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('questions.urls'), name='questions_urls')
+    path('', include('questions.urls'), name='questions_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
