9e63c17ba7fae2cb7f93c6ecb22cf5ff336dcc61
josswe26 <josec.sanchez@icloud.com>
2022-03-15 00:02:05 +0000

    Install and configure django_summernote

--- questions/admin.py
+++ questions/admin.py
@@ -1,19 +1,24 @@
 from django.contrib import admin
 from .models import Question, Reply
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'author', 'created_on', 'last_updated')
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('created_on', 'last_updated')
     search_fields = ('title', 'content', 'author__username')
 
+    summernote_fields = ('content')
+
 
 @admin.register(Reply)
-class ReplyAdmin(admin.ModelAdmin):
+class ReplyAdmin(SummernoteModelAdmin):
 
     list_display = ('body', 'author', 'created_on', 'last_updated')
     list_filter = ('created_on', 'last_updated')
     search_fields = ('body', 'author__username')
+
+    summernote_fields = ('body')
