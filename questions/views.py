a90180929a5bda9ec6985b741a157019b2762008
josswe26 <josec.sanchez@icloud.com>
2022-03-16 22:03:08 +0000

    Add method to post QuestionForm

--- questions/views.py
+++ questions/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
+from django.utils.text import slugify
 from .models import Question
 from .forms import QuestionForm
 
 
 class QuestionListView(generic.ListView):
-    """ List all questions and its content """
-    
+    """ List view to render all questions """
+
     model = Question
     paginate_by = 10
 
@@ -14,16 +16,31 @@ class QuestionListView(generic.ListView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
+        """ Get question list and question form and return data for rendering"""
         context = super(QuestionListView, self).get_context_data(**kwargs)
         context['question_form'] = QuestionForm()
         return context
 
+    def post(self, request, *args, **kwargs):
+        """ Post method for QuestionForm """
+        question_form = QuestionForm(data=request.POST)
+
+        if question_form.is_valid():
+            question_form.instance.author = request.user
+            question_form.instance.author_id = request.user.id
+            question_form.instance.slug = slugify(question_form.instance.title)
+            question_form.save()
+        else:
+            question_form = QuestionForm()
+
+        return HttpResponseRedirect(reverse('home'))
+
 
 class QuestionDetailView(View):
-    """ Display detailed information about a specific question """
+    """  View to render detailed information of a specific question """
 
     def get(self, request, slug, *args, **kwargs):
-
+        """ Get question information and return data for rendering """
         queryset = Question.objects.all()
         question = get_object_or_404(queryset, slug=slug)
         replies = question.replies.all().order_by('created_on')
