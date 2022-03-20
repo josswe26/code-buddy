ecfd698c1083f1ab0a9c5dc30588a94fd022845e
josswe26 <josec.sanchez@icloud.com>
2022-03-20 22:01:34 +0000

    Fix QuestionDeleteView url

--- questions/urls.py
+++ questions/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.QuestionListView.as_view(), name='home'),
     path('<slug:slug>/', views.QuestionDetailView.as_view(), name='question_detail'),
     path('edit_question/<int:id>/', views.QuestionEditView.as_view(), name='edit_question'),
-    path('delete_question/<int:id>/', views.QuestionDeleteView.as_view(), name='delete_question')
+    path('delete_question/<int:id>/', views.QuestionDeleteView.as_view(), name='delete_question'),
 ]
