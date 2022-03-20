d6703545ce785d966ae1f8947063ef68ebfb1c5e
josswe26 <josec.sanchez@icloud.com>
2022-03-20 19:24:52 +0000

    Create url to QuestionDeleteView

--- questions/urls.py
+++ questions/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.QuestionListView.as_view(), name='home'),
     path('<slug:slug>/', views.QuestionDetailView.as_view(), name='question_detail'),
-    path('edit_question/<int:id>/', views.QuestionEditView.as_view(), name='edit_question')
+    path('edit_question/<int:id>/', views.QuestionEditView.as_view(), name='edit_question'),
+    path('delete_question/<int:id>/', views.QuestionDeleteView.as_view(), name='delete_question')
 ]
