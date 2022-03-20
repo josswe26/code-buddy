2c34e981faaed84cc2e34c932b3014cc90541daa
josswe26 <josec.sanchez@icloud.com>
2022-03-20 19:04:53 +0000

    Create url to QuestionEditView

--- questions/urls.py
+++ questions/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.QuestionListView.as_view(), name='home'),
-    path('<slug:slug>/', views.QuestionDetailView.as_view(), name='question_detail')
+    path('<slug:slug>/', views.QuestionDetailView.as_view(), name='question_detail'),
+    path('edit_question/<int:id>/', views.QuestionEditView.as_view(), name='edit_question')
 ]
