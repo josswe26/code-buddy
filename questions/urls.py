421081b5cf9d8d670a514c17db6e4fd090f3f879
josswe26 <josec.sanchez@icloud.com>
2022-03-20 22:03:33 +0000

    Create ReplyEditView url

--- questions/urls.py
+++ questions/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<slug:slug>/', views.QuestionDetailView.as_view(), name='question_detail'),
     path('edit_question/<int:id>/', views.QuestionEditView.as_view(), name='edit_question'),
     path('delete_question/<int:id>/', views.QuestionDeleteView.as_view(), name='delete_question'),
+    path('edit_reply/<int:id>/', views.ReplyEditView.as_view(), name='edit_reply'),
 ]
