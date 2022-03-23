562f39e81bbf9020f37b1a3f3cc8fca716296fb1
josswe26 <josec.sanchez@icloud.com>
2022-03-23 20:53:37 +0000

    Add view code for reply voting

--- questions/views.py
+++ questions/views.py
@@ -14,7 +14,7 @@ class QuestionListView(generic.ListView):
     model = Question
     paginate_by = 10
 
-    queryset = Question.objects.all().order_by('-last_updated')
+    queryset = Question.objects.all().order_by('-created_on')
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
@@ -35,7 +35,7 @@ class QuestionListView(generic.ListView):
             question_form = QuestionForm()
 
         return HttpResponseRedirect(reverse('home'))
-
+        
 
 class QuestionDetailView(View):
     """  View to render detailed information of a specific question """
@@ -44,7 +44,7 @@ class QuestionDetailView(View):
         """ Get question information and return data for rendering """
         queryset = Question.objects.all()
         question = get_object_or_404(queryset, slug=slug)
-        replies = question.replies.all().order_by('last_updated')
+        replies = question.replies.all().order_by('-votes_score')
         
         return render(
             request,
@@ -249,3 +249,29 @@ class VoteQuestion(View):
         if request.POST.get('location') == 'question_detail':
             return HttpResponseRedirect(reverse('question_detail', args=[question.slug]))
 
+
+class VoteReply(View):
+    """ Vote Reply view """
+
+    def post(self, request, id):
+        """ Register reply votes """
+
+        vote = ReplyVote()
+        reply = get_object_or_404(Reply, id=id)
+
+        score = request.POST.get('vote_score')
+
+        vote, created = ReplyVote.objects.update_or_create(
+            voter=request.user,
+            reply=reply,
+            defaults={'score': score},
+        )
+
+        vote.save()
+
+        total_score = ReplyVote.objects.filter(reply=reply).aggregate(Sum('score'))
+
+        reply.votes_score = total_score['score__sum']
+        reply.save()
+
+        return HttpResponseRedirect(reverse('question_detail', args=[reply.question.slug]))
