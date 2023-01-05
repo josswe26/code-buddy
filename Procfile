82f6bd3a0a1cface27da4f6bf4871f65749764ea
josswe26 <josec.sanchez@icloud.com>
2023-01-05 00:12:48 +0000

    Set up for deployment on Render

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn code_buddy.wsgi
--- code_buddy/settings.py
+++ code_buddy/settings.py
@@ -33,7 +33,11 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['django-code-buddy.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['localhost']
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
